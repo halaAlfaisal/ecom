e4be4fb99750c8331530e5a716f71dc56eedf7b9
halaAlfaisal <halfaisal.ce@gmail.com>
2023-09-16 22:05:53 +0300

    trys

--- app/models/item.server.ts
+++ app/models/item.server.ts
@@ -1,63 +1,89 @@
 import { prisma } from "~/db.server";
 
 export async function getItems() {
-  return prisma.item.findMany();
+  try {
+    return await prisma.item.findMany();
+  } catch (error) {
+    console.error("Error in getItems:", error);
+    return [];
+  }
 }
 
 export async function createOrder(cartItems: CartItem[]) {
-  let total = cartItems
-    .reduce((sum, item) => {
-      return sum + item.price * item.quantity;
-    }, 0)
-    .toFixed(3);
+  try {
+    let total = cartItems
+      .reduce((sum, item) => {
+        return sum + item.price * item.quantity;
+      }, 0)
+      .toFixed(3);
 
-  const createdOrder = await prisma.order.create({
-    data: {
-      total,
-    },
-  });
-
-  for (const item of cartItems) {
-    await prisma.orderItem.create({
+    const createdOrder = await prisma.order.create({
       data: {
-        itemId: item.id,
-        name: item.name,
-        price: item.price,
-        quantity: item.quantity,
-        description: item.description,
-        imgSrc: item.imgSrc,
-        orderId: createdOrder.id,
+        total,
       },
     });
-  }
 
-  return createdOrder;
+    for (const item of cartItems) {
+      await prisma.orderItem.create({
+        data: {
+          itemId: item.id,
+          name: item.name,
+          price: item.price,
+          quantity: item.quantity,
+          description: item.description,
+          imgSrc: item.imgSrc,
+          orderId: createdOrder.id,
+        },
+      });
+    }
+
+    return createdOrder;
+
+  } catch (error) {
+    console.error("Error in createOrder:", error);
+    return null;
+  }
 }
 
 export async function getOrders() {
-  return prisma.order.findMany({
-    include: {
-      items: true,
-    },
-  });
+  try {
+    return await prisma.order.findMany({
+      include: {
+        items: true,
+      },
+    });
+  } catch (error) {
+    console.error("Error in getOrders:", error);
+    return null;
+  }
 }
 
 export async function getOrderById(id: string) {
-  return prisma.order.findUnique({
-    where: {
-      id,
-    },
-    include: {
-      items: true,
-    },
-  });
+  try {
+    return await prisma.order.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        items: true,
+      },
+    });
+  } catch (error) {
+    console.error("Error in getOrderById:", error);
+    return null;
+  }
 }
 
 export async function updateOrderStatus(orderId: string, status: string) {
-  return prisma.order.update({
-    where: {
-      id: orderId,
-    },
-    data: { status },
-  });
+  try {
+    return await prisma.order.update({
+      where: {
+        id: orderId,
+      },
+      data: { status },
+    });
+  } catch (error) {
+    console.error("Error in updateOrderStatus:", error);
+    return null;
+  }
 }
